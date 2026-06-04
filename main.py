a974cb00ad2d62e3283342b73dfafb2600afb59c
randywporter <randywporter@icloud.com>
2026-06-04 09:55:19 -0400

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Machine learning and data analytics project
+Collects data from the Hypixel skyblock Bazaar
+and then is trained on good flips in order to,
+eventually, recommend good flips within a dashboard
+interface, probably a web interface.
+'''
+
+print("hello world!")
